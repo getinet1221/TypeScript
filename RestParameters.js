55049e2a208992377e1f6011d6ea2a4e5746e253
Getinet Amare <getinet.dtu@outlook.com>
2022-01-05 16:34:40 +0300

    Adding indefinite parameters demo in typescript

---
+++ RestParameters.js
@@ -0,0 +1,21 @@
+// indefinite number of arguments as an array in typescript
+// function fn(...rest: type[]) {
+//     //...
+//  }
+// its syntax is this 
+function IndefiniteDemo() {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    var total = 0;
+    for (var i = 0; i < numbers.length; i++) {
+        return total += numbers[i];
+    }
+}
+// lets call the indefinite function right here
+console.log("Get total With one argument : " + IndefiniteDemo(12));
+console.log("Get tota With two argument : " + IndefiniteDemo(10, 20));
+console.log("Get tota With three argument : " + IndefiniteDemo(30, 40, 50));
+console.log("Get tota With four argument : " + IndefiniteDemo(100, 200, 300, 400));
+console.log("Get tota With five argument : " + IndefiniteDemo(500, 600, 700, 800, 900));
---
+++ RestParameters.ts
@@ -0,0 +1,19 @@
+// indefinite number of arguments as an array in typescript
+// function fn(...rest: type[]) {
+//     //...
+//  }
+// its syntax is this 
+function IndefiniteDemo(...numbers : number[]):number{
+    var total = 0;
+    for(let i=0;i<numbers.length;i++){
+        return total+=numbers[i];
+    }
+}
+// lets call the indefinite function right here
+console.log("Get total With one argument : "+IndefiniteDemo(12));
+console.log("Get tota With two argument : "+IndefiniteDemo(10,20));
+console.log("Get tota With three argument : "+IndefiniteDemo(30,40,50));
+console.log("Get tota With four argument : "+IndefiniteDemo(100,200,300,400));
+console.log("Get tota With five argument : "+IndefiniteDemo(500,600,700,800,900));
+
+
